e9511f20d545da86129b5ce62147dc541cb69719
Sem8 <mssemmi8@gmail.com>
2019-04-11 23:23:46 -0400

    post request endpoint created

--- routers/crossfit-router.js
+++ routers/crossfit-router.js
@@ -1,6 +1,7 @@
 const crossfitRouter = require("express").Router();
 
 const crossfitdb = require("../database/dbConfig.js");
+const crossfitWods = require("../helpers/crossfit-model.js");
 
 crossfitRouter.get("/", (req, res) => {
   crossfitdb("crossfitwod")
@@ -15,6 +16,24 @@ crossfitRouter.get("/", (req, res) => {
   // res.send('Welcome to your Crossfit WOD center')
 });
 
-crossfitRouter.post("/");
+crossfitRouter.post("/", async (req, res) => {
+  const { Workout_Name, Warmup, Workout_Structure, Workout } = req.body;
+  if (!Workout_Name && !Warmup && !Workout_Structure && !Workout) {
+    res
+      .status(400)
+      .json({ message: `Bad request, submit all required fields` });
+  } else {
+    try {
+      const newCrossfitWod = req.body;
+      crossfitWods.addCrossfitWod(newCrossfitWod).then(crossfitWod => {
+        res.status(201).json(crossfitWod);
+      });
+    } catch (error) {
+      res.status(500).json({
+        error: `There was an error while saving the crossfit workout to the database: ${error}`
+      });
+    }
+  }
+});
 
 module.exports = crossfitRouter;
