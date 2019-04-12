a53920ba512c81ffa9ab69af678ff792b78bebca
Sem8 <mssemmi8@gmail.com>
2019-04-11 23:41:49 -0400

    crossfit workout get by id endpoint made

--- routers/crossfit-router.js
+++ routers/crossfit-router.js
@@ -16,6 +16,23 @@ crossfitRouter.get("/", (req, res) => {
   // res.send('Welcome to your Crossfit WOD center')
 });
 
+crossfitRouter.get("/:id", (req, res) => {
+  const { id } = req.params;
+  crossfitdb("crossfitwod")
+    .where({ id })
+    .first()
+    .then(crossfitWod => {
+      res.status(200).json(crossfitWod);
+    })
+    .catch(error => {
+      res
+        .status(500)
+        .json({
+          message: `Error occurred while retrieving crossfit workout: ${error}`
+        });
+    });
+});
+
 crossfitRouter.post("/", async (req, res) => {
   const { Workout_Name, Warmup, Workout_Structure, Workout } = req.body;
   if (!Workout_Name && !Warmup && !Workout_Structure && !Workout) {
@@ -33,6 +50,23 @@ crossfitRouter.post("/", async (req, res) => {
         error: `There was an error while saving the crossfit workout to the database: ${error}`
       });
     }
+  }
+});
+
+crossfitRouter.delete("/:id", async (req, res) => {
+  try {
+    const count = await crossfitdb("crossfitwod")
+      .where({ id: req.params.id })
+      .debug();
+    if (count > 0) {
+      res.status(204).end();
+    } else {
+      res.status(404).json({ message: "Crossfit workout not found" });
+    }
+  } catch (error) {
+    res.status(500).json({
+      error: `Error occurred while deleting this crossfit workout: ${error}`
+    });
   }
 });
 
