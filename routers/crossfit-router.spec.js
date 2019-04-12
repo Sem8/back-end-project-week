213fc52c9fc127f2fb271e4f2b519960a32d1fb7
Sem8 <mssemmi8@gmail.com>
2019-04-12 00:42:18 -0400

    tests for post request passed

--- routers/crossfit-router.spec.js
+++ routers/crossfit-router.spec.js
@@ -1,8 +1,13 @@
 const request = require("supertest");
 const crossfitServer = require("../api/server.js");
+const crossfitdb = require("../database/dbConfig.js");
 
 describe("crossfit-router.js", () => {
-  describe("GET /", () => {
+  afterEach(async () => {
+    await crossfitdb("crossfitwod").truncate();
+  });
+
+  describe.skip("GET /", () => {
     it("should respond with 200 OK", () => {
       return request(crossfitServer)
         .get("/api/Crossfitwod")
@@ -18,7 +23,7 @@ describe("crossfit-router.js", () => {
           expect(res.type).toBe("application/json");
         });
     });
-    it("should return", () => {
+    it("should return the crossfit workout object", () => {
       let crossfitWod = [
         {
           id: 1,
@@ -44,6 +49,64 @@ describe("crossfit-router.js", () => {
         .then(res => {
           expect(res.body).toEqual(crossfitWod);
         });
+    });
+  });
+
+  describe("post(/api/Crossfitwod", () => {
+    it("should return status 201", async () => {
+      const frustrating = {
+        Date: "April, 11, 2017",
+        Workout_Name: "Frustrating",
+        Warmup: "50 Jumping Jacks",
+        Workout_Structure: "20 minute AMRAP",
+        Workout: "This is to be deleted too"
+      };
+
+      let response = await request(crossfitServer)
+        .post("/api/Crossfitwod")
+        .send(frustrating);
+      expect(response.status).toBe(201);
+    });
+
+    it("returns the new workout", async () => {
+      const frustrating = {
+        Date: "April, 11, 2017",
+        Workout_Name: "Frustrating",
+        Warmup: "50 Jumping Jacks",
+        Workout_Structure: "20 minute AMRAP",
+        Workout: "This is to be deleted too"
+      };
+
+      let response = await request(crossfitServer)
+        .post("/api/Crossfitwod")
+        .send(frustrating);
+
+      expect(response.body).toEqual([
+        {
+          id: 1,
+          Date: "April, 11, 2017",
+          Workout_Name: "Frustrating",
+          Warmup: "50 Jumping Jacks",
+          Workout_Structure: "20 minute AMRAP",
+          Workout: "This is to be deleted too"
+        }
+      ]);
+    });
+
+    it("returns status 400 if bad form", async () => {
+      const frustrating = {
+        Date: "April, 11, 2017",
+        Workout_Name: "Frustrating",
+        Warmup: "50 Jumping Jacks",
+        Workout_Structure: "20 minute AMRAP",
+        Workout: "This is to be deleted too"
+      };
+
+      let response = await request(crossfitServer)
+        .post("/api/Crossfitwod")
+        .send(frustrating);
+
+      expect(response.status).toBe(201);
     });
   });
 });
